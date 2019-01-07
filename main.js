4c92ba818baa8cb8a374359845ffabde8fa702c6
ormxmi <36085168+ormxmi@users.noreply.github.com>
2019-01-08 01:32:07 +0200

    etc

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var offsetTop = document.getElementsByClassName("board")[0].getBoundingClientRect().top;
 var offsetLeft = document.getElementsByClassName("board")[0].getBoundingClientRect().left;
 var squareClickedX, squareClickedY;
-selectedPieceBool = false; selectedPieceCoords = []; var playerTurn = "w"; 
+selectedPieceBool = false; selectedPieceCoords = []; var playerTurn = "w"; var whiteKingChecked = false;var legalMoves = []; var blackKingChecked = false;
 
 var board = [[['R','w',[0,0]],['N','w',[0,1]],['B','w',[0,2]],['Q','w',[0,3]],['K','w',[0,4]],['B','w',[0,5]],['N','w',[0,6]],['R','w',[0,7]]],[['P','w',[1,0],true],['P','w',[1,1],true],['P','w',[1,2],true],['P','w',[1,3],true],['P','w',[1,4],true],['P','w',[1,5],true],['P','w',[1,6],true],['P','w',[1,7],true]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[['P','b',[6,0],true],['P','b',[6,1],true],['P','b',[6,2],true],['P','b',[6,3],true],['P','b',[6,4],true],['P','b',[6,5],true],['P','b',[6,6],true],['P','b',[6,7],true]],[['R','b',[7,0]],['N','b',[7,1]],['B','b',[7,2]],['Q','b',[7,3]],['K','b',[7,4]],['B','b',[7,5]],['N','b',[7,6]],['R','b',[7,7]]]]
 let piecesWidth = 64;
@@ -18,33 +18,42 @@ init();///////
 // 
 // 
 function kingXray(pos){
+    legalMoves = [];
     let counterSameTeamPieces = 0;
     for(var i = pos[0]+1;i<=7;i++){
-        if(board[i][pos[1]][1]!==playerTurn&&board[i][pos[1]][1]!==undefined){
+        legalMoves.push([i,pos[1]]);
+        if(board[i][pos[1]][1]==playerTurn&&board[i][pos[1]][1]!==undefined){
             if(board[i][pos[1]][0]=='R'||board[i][pos[1]][0]=='Q'){
-             
-                console.log("a threat! a queen or rook pinning!");
+                if(counterSameTeamPieces==0){
+                    console.log(playerTurn+"'s king is checked..");
+                    whiteKingChecked = true;
+                    console.log(legalMoves);
+                }
+                else{
+                    console.log("a threat! a queen or rook pinning!");
+                }
             }
             else{
-                console.log(board[i][pos[1]][0]);
                 console.log("not a threat! not queen or rook!");
             }
             return false; // after 1st enemy encounter
-           
         }
         else{
-            if(board[i][pos[1]][1]==playerTurn&&board[i][pos[1]][1]!==undefined){
+            if(board[i][pos[1]][1]!==playerTurn&&board[i][pos[1]][1]!==undefined){
+                console.log(board[i][pos[1]]);
                 counterSameTeamPieces++;
                 if(counterSameTeamPieces>=2){
                     console.log("not a threat! 2 pieces ahead");
+                    counterSameTeamPieces = 0;
                     return false;
                 }
 
             }
         }
+        
     }
 }
-kingXray([0,4]);
+
 //function atLeastTwoFriendlyPiecesAhead(pos,forEnd,increment){
 //    let counterFriendlyPieces = 0;
 //    for(var i = pos[0]+1;i < forEnd;i = i + increment){
@@ -61,7 +70,7 @@ document.getElementsByClassName("board")[0].onclick = function(e){
             if(board[squareClickedY][squareClickedX][1]!==playerTurn) selectedPieceBool = false;
         }
         else{
-           
+            
                 if(movePiece(selectedPieceCoords,[squareClickedY,squareClickedX],playerTurn)){
                     let currentPiece =  document.getElementById(selectedPieceCoords[0].toString()+selectedPieceCoords[1]);
                     if(currentPiece.style.transform==""){
@@ -74,6 +83,7 @@ document.getElementsByClassName("board")[0].onclick = function(e){
                         currentPiece.style.transform = "translate("+(squareClickedX-board[currentPiece.id[0]][currentPiece.id[1]][2][1])+"00%, -"+(squareClickedY-board[currentPiece.id[0]][currentPiece.id[1]][2][0])+"00%)"; 
                     }
                         selectedPieceBool = false;
+                        
                         playerTurn = "b";
                 }
                 else{
@@ -101,6 +111,13 @@ document.getElementsByClassName("board")[0].onclick = function(e){
                     currentPiece.style.transform = "translate("+(squareClickedX-board[currentPiece.id[0]][currentPiece.id[1]][2][1])+"00%, "+Math.abs(squareClickedY-board[currentPiece.id[0]][currentPiece.id[1]][2][0])+"00%)"; 
                 }
                     selectedPieceBool = false;
+                    for(var y = 0; y<=7;y++){
+                        for(var x = 0; x<=7;x++){
+                            if(board[y][x][0]=='K'&&board[y][x][1]=='w'){
+                                kingXray([y,x]);
+                            }
+                        }
+                    }
                     playerTurn = "w";
             }
             else{
@@ -119,6 +136,21 @@ function selectPiece(y,x){
 
 function movePiece(from,to,playerTurn){
     if(isLegalMove(from,to,playerTurn)){
+        if(playerTurn=='w'&&whiteKingChecked){
+            for(var c = 0; c<legalMoves.length;c++){
+                console.log("["+to[0]+","+to[1]+"]",legalMoves[c]);
+                if(to[0]==legalMoves[c][0]&&to[1]==legalMoves[c][1]){
+                    
+                    console.log("legal move!");
+                    board[to[0]][to[1]] = board[from[0]][from[1]];
+                    board[from[0]][from[1]]=[];
+                    return true;
+                }
+                
+            }
+                    console.log("white king is under check!");
+                    return false;
+        }
         console.log("legal move!");
         board[to[0]][to[1]] = board[from[0]][from[1]];
         board[from[0]][from[1]]=[];
@@ -178,10 +210,11 @@ function moveOrTake(playerMoving,square){
     }
 }
 function moveAndTake(playerMoving,square){
-    let pieceToRemove =  document.getElementById(square[0].toString()+square[1].toString());
-    setTimeout(()=>{
-        pieceToRemove.outerHTML = "";
-    },150)
+    
+        let pieceToRemove =  document.getElementById(square[0].toString()+square[1].toString());
+        setTimeout(()=>{
+            pieceToRemove.outerHTML = "";
+        },150)
     
     
 }
